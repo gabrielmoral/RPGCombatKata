30ac80a721b49db12046e378d5c08503acac373c
Gabriel Moral <gabrielmoralortiz@gmail.com>
2016-07-25 12:21:40 +0200

    First iteration

---
+++ RPGCombatKata_charp.Test/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentAssertions;
+using RPGCombatKata_csharp;
+using Xunit;
+
+namespace RPGCombatKata_charp.Test
+{
+	public class Tests
+	{
+		private int minimum_damage_attack_to_die_character = 1000;
+
+		[Fact]
+		public void When_Damage_Is_Higher_Than_Health_The_Character_Dies()
+		{
+			var character = new Character();
+
+			character.RecieveDamage(minimum_damage_attack_to_die_character);
+
+			character.IsDead().Should().BeTrue();
+		}
+
+		[Fact]
+		public void When_The_Character_Is_Dead_Cannot_Be_Healed()
+		{
+			int healingPoints = 20;
+
+			var character = new Character();
+
+			character.RecieveDamage(minimum_damage_attack_to_die_character);
+			character.RecieveHealing(healingPoints);
+
+			character.IsDead().Should().BeTrue();
+		}
+
+		[Fact]
+		public void Character_Cannot_Exceed_Maximum_Health()
+		{
+			int healingPoints = 100;
+
+			var character = new Character();
+
+			character.RecieveHealing(healingPoints);
+
+			character.CurrentHealth.Should().Be(Character.MaximumHealth);
+		}
+	}
+}
+
---
+++ RPGCombatKata_csharp/Character.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RPGCombatKata_csharp
+{
+	public class Character
+	{
+		private const int MinimumHealth = 0;
+		public const int MaximumHealth = 1000;
+
+		private int currentLevel = 1;
+
+		public int CurrentHealth { get; private set;}
+
+		public Character()
+		{
+			this.CurrentHealth = MaximumHealth;
+		}
+
+		public void RecieveDamage(int damage)
+		{
+			HurtMyself(damage);
+		}
+
+		public void RecieveHealing(int healing)
+		{
+			if (IsDead()) return;
+
+			HealMyself(healing);
+		}
+
+		public bool IsDead()
+		{
+			return this.CurrentHealth == MinimumHealth;
+		}
+
+		void HurtMyself(int damage)
+		{
+			this.CurrentHealth -= damage;
+
+			if (this.CurrentHealth < MinimumHealth)
+			{
+				this.CurrentHealth = MinimumHealth;
+			}
+		}
+
+		void HealMyself(int healing)
+		{
+			CurrentHealth += healing;
+
+			if (this.CurrentHealth > MaximumHealth)
+			{
+				this.CurrentHealth = MaximumHealth;
+			}
+		}
+	}
+}
+
